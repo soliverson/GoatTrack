b3c6e2690ee22f860d3cb683d2a17740f491bf01
soliverson <stacy.oliverson@gmail.com>
2024-07-10 19:22:36 -0600

    Update page

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'client',
-  publicDir: 'public',
-  server: {
-    port: 3000  // Changed port from 3001 to 3000
-  },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: 'public/index.html'
-    }
-  }
+  },
+  server: {
+    port: 3000,
+  },
 });
