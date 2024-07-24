d85cf23d20f34f15f733d6296d40123967c24111
soliverson <stacy.oliverson@gmail.com>
2024-07-24 09:39:46 -0600

    update

--- server/server.js
+++ server/server.js
@@ -8,6 +8,22 @@ const apiRoutes = require('./routes/apiRoutes');
 app.use(express.static(path.join(__dirname, 'client', 'public')));
 app.use('/api', apiRoutes);
 
+app.get('/goat-data', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'public', 'goat-data.html'));
+});
+
+app.get('/goat-profile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'public', 'goat-profile.html'));
+});
+
+app.get('/community-forum', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'public', 'community-forum.html'));
+});
+
+app.get('/goat-breeds', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'public', 'goat-breeds.html'));
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
 });
