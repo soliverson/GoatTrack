10d29c8a45e55ef894f76fc4207796bd058f587e
soliverson <stacy.oliverson@gmail.com>
2024-07-24 08:36:29 -0600

    added goat-breeds

--- client/public/js/router.js
+++ client/public/js/router.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const router = async () => {
     const routes = [
-      { path: "/", view: () => loadHTMLContent("home.html") },
+      { path: "/", view: () => loadHTMLContent("home-content.html") },
       { path: "/goat-data", view: () => loadHTMLContent("goat-data.html") },
       { path: "/goat-profile", view: () => loadHTMLContent("goat-profile.html") },
       { path: "/community-forum", view: () => loadHTMLContent("community-forum.html") },
@@ -36,9 +36,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const loadHTMLContent = async (url) => {
     const mainContent = document.getElementById('main-content');
-    const response = await fetch(url);
-    const data = await response.text();
-    mainContent.innerHTML = data;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.text();
+      mainContent.innerHTML = data;
+    } catch (error) {
+      console.error('Failed to load HTML content:', error);
+      mainContent.innerHTML = `<p>Failed to load content. Please try again later.</p>`;
+    }
   };
 
   window.addEventListener('popstate', router);
